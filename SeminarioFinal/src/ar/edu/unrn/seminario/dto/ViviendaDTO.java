de070f93ce6925023ee31f57f0a8c0be3965944c
Sektor <Sektor@192.168.1.175>
2021-09-02 18:06:33 -0300

    Correccion

--- SeminarioFinal/src/ar/edu/unrn/seminario/dto/ViviendaDTO.java
+++ SeminarioFinal/src/ar/edu/unrn/seminario/dto/ViviendaDTO.java
@@ -2,13 +2,13 @@ package ar.edu.unrn.seminario.dto;
 
 import java.util.Date;
 
-public class ViviendaRTO {
+public class ViviendaDTO {
 
 	private DueñoViviendaDTO dueño;
 	private DomicilioDTO domicilio;
 	private Date fechaRegistro;
 
-	public ViviendaRTO(DueñoViviendaDTO dueño, DomicilioDTO domicilio, Date fechaRegistro) {
+	public ViviendaDTO(DueñoViviendaDTO dueño, DomicilioDTO domicilio, Date fechaRegistro) {
 		this.dueño = dueño;
 		this.domicilio = domicilio;
 		this.fechaRegistro = fechaRegistro;
